b35f2704e34ef03ede8db7cf4aa122cee2c6ddb8
kevinhsu <kevinhsu@synology.com>
2018-07-11 10:31:53 +0800

    Fix loading mask

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -23,8 +23,6 @@
 			$all = $body.add($header);
 
 		// Disable animations/transitions until the page has loaded.
-			$body.addClass('is-loading');
-
 			$window.on('load', function() {
 				window.setTimeout(function() {
 					$body.removeClass('is-loading');
